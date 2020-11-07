d1b1cf982c0e6d7c90872c6629a246aa46feea52
Zararest <71099190+Zararest@users.noreply.github.com>
2020-11-07 18:23:30 +0300

    Add files via upload

--- Onegin with list.cpp
+++ Onegin with list.cpp
@@ -4,6 +4,7 @@
 #include <errno.h>
 #include <sys/stat.h>
 #include <math.h>
+#include <clocale>
 
 const int MAXLEN = 400;
 
@@ -67,18 +68,71 @@ int my_strcmp(char* str1, char* str2)
 }
 
 
+int str_len(char* str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return i;
+}
+
+int my_strcmp_rev(char* str1, char* str2)
+{
+    int i = 0, j =0;
+    i = str_len(str1);
+    j = str_len(str2);
+
+    while ( (str1[i] == str2[j]) && ( j > 0 ) && ( i > 0 ) )
+    {
+        i--;
+        j--;
+    }
+
+    if ( (i == 0) && (j == 0) )
+    {
+        return 0;
+    } else
+    {
+        if (i == 0)
+        {
+            return 1;
+        }
+
+        if (j == 0)
+        {
+            return -1;
+        }
+
+
+        if ( ( str1[i] > str2[j] ) && (i != 0) && (j != 0) )
+        {
+            return 1;
+        }
+        if ( ( str1[i] <= str2[j] ) && (i != 0) && (j != 0) )
+        {
+            return -1;
+        }
+
+    }
+
+}
+
+
 void add_to_mas(int pos_of_val, struct note **root_of_mas, char* val)
 {
     struct note* tmp = (struct note*) calloc (1, sizeof(struct note));
     struct note* point = (struct note*) calloc (1, sizeof(struct note));
-    char* val_tmp = (char*) calloc (len_str(val) + 1, sizeof(char));
+    //char* val_tmp = (char*) calloc (len_str(val) + 1, sizeof(char));
     int i = 0;
 
-    my_copy(val, val_tmp);
+    //my_copy(val, val_tmp);
 
     if (pos_of_val == 1)
     {
-        tmp->inf = val_tmp;
+        tmp->inf = val;
         tmp->p = *root_of_mas;
 
         *root_of_mas = tmp;
@@ -99,7 +153,7 @@ void add_to_mas(int pos_of_val, struct note **root_of_mas, char* val)
         }
 
         tmp->p = point->p;
-        tmp->inf = val_tmp;
+        tmp->inf = val;
         point->p = tmp;
 
     }
@@ -248,6 +302,71 @@ void sort_my(int sort_from, int sort_to, struct note **root_of_mas)
 
 }
 
+
+
+void sort_my_reverse(int sort_from, int sort_to, struct note **root_of_mas)
+{
+
+    int main_num = round( (sort_from + sort_to)/2 );
+    char* main_val = val(main_num, root_of_mas);
+    int i = 0;
+
+    if (sort_to > (sort_from + 1) )
+    {
+        for (i = sort_from; i < main_num; i++)
+        {
+
+            if ( my_strcmp_rev( val(i, root_of_mas), main_val ) == 1 )
+            {
+                if ( (main_num + 1) > sort_to )
+                {
+                    insert_and_del(i, main_num - 1, root_of_mas);
+                } else
+                {
+                    insert_and_del(i, main_num, root_of_mas);
+                }
+                main_num--;
+                i--;
+            }
+        }
+
+
+        for (i = (main_num + 1); i <= sort_to; i++)
+        {
+
+            if ( my_strcmp_rev( val(i, root_of_mas), main_val) == -1 )
+            {
+                if ( (main_num - 1) < sort_from)
+                {
+                    insert_and_del(i, main_num, root_of_mas);
+                } else
+                {
+                    insert_and_del(i, main_num - 1, root_of_mas);
+                }
+                main_num++;
+            }
+        }
+
+        sort_my_reverse(sort_from, (main_num - 1), root_of_mas);
+        sort_my_reverse( (main_num), sort_to, root_of_mas);
+    } else
+    {
+        if (sort_to == (sort_from + 1))
+        {
+            if ( my_strcmp_rev( val(sort_from, root_of_mas), val(sort_to, root_of_mas)) == 1)
+            {
+                insert_and_del(sort_from, (sort_from + 1), root_of_mas );
+            }
+        }
+    }
+
+
+
+}
+
+
+
+
 void zero_char(char str[MAXLEN])
 {
     int i = 0;
@@ -293,6 +412,7 @@ void my_insert(char from_str[MAXLEN], char* to_str)
 
 void fprint_list(int from, int to, struct note **root_of_mas, char name_of_output_file[MAXLEN])
 {
+    char *locale = setlocale( LC_ALL,"Russian" );
     int i = 1;
     FILE* p_output = fopen(name_of_output_file, "w");
     struct note* point = (struct note*) calloc (1, sizeof(struct note));
@@ -319,7 +439,7 @@ void fprint_list(int from, int to, struct note **root_of_mas, char name_of_outpu
         if (point->p != NULL)
         {
             point = point->p;
-            fprintf(p_output, "%s", point->inf);
+            fprintf(p_output, "%s\n", point->inf);
         } else
         {
             printf("in val() smt went wrong on i = %i\n", i);
@@ -332,18 +452,23 @@ void fprint_list(int from, int to, struct note **root_of_mas, char name_of_outpu
 }
 
 
-void file_sort(char name_of_input_file[MAXLEN], char name_of_output_file[MAXLEN])
+void file_sort(char name_of_input_file[MAXLEN], char name_of_output_file[MAXLEN], int sort_type)
 {
+    char *locale = setlocale( LC_ALL,"Russian" );
     FILE* p_input = fopen(name_of_input_file, "r");
 
+    int i = 0, num_of_str = 0;
+    struct stat statistika;
+
+    stat(name_of_input_file, &statistika);
+    printf("stat == %i\n", statistika.st_size + 1);
+    char* buffer = (char*) calloc(statistika.st_size + 1, sizeof(char));
     char symbol = '-1';
     struct note* mas_of_point = NULL;
-    char str[MAXLEN] = {'\0'};
-    char* point_str = NULL;
+    char* point_str = &buffer[0];
 
 
-    int i = 0, num_of_str = 0;
-    struct stat statistika = {0};
+
 
     if ( p_input == NULL)
     {
@@ -381,32 +506,33 @@ void file_sort(char name_of_input_file[MAXLEN], char name_of_output_file[MAXLEN]
         if ( (symbol != '\n') && (symbol != EOF) )
         {
 
-            if (i > (MAXLEN - 1))
-            {
-                printf("The string N%i  is too long", num_of_str);
-                exit(-1);
-            }
-            str[i] = symbol;
+            buffer[i] = symbol;
             i++;
         } else
         {
-            point_str = (char*) calloc(i + 2, sizeof(char) );
-
-            my_insert(str, point_str);
-
-            point_str[i] = '\n';
-            point_str[i + 1] = '\0';
+            printf("i love DRAM\n");
+            buffer[i] = '\0';
+            i++;
 
             add_to_mas(1, &mas_of_point, point_str);
             num_of_str++;
-            i = 0;
-            zero_char(str);
+
+            point_str = &buffer[i];
+
         }
 
 
     }
 
-    sort_my(1, num_of_str, &mas_of_point);
+    if (sort_type == 1)
+    {
+        sort_my(1, num_of_str, &mas_of_point);
+    } else
+    {
+        printf("==%i==", sort_type);
+        sort_my_reverse(1, num_of_str, &mas_of_point);
+    }
+
 
     fprint_list(1, num_of_str, &mas_of_point, name_of_output_file);
 
@@ -417,14 +543,36 @@ void file_sort(char name_of_input_file[MAXLEN], char name_of_output_file[MAXLEN]
 
 int main ()
 {
+    char *locale = setlocale(LC_ALL, "Russian");
     char name_of_input_file[MAXLEN], name_of_output_file[MAXLEN];
+    char input_check;
+    int sort_type = 1;
 
     printf("Do you want to sort the novel 'Eugene Onegin'?\n"
            "(Press ENTER to do that)\n");
 
     while ( getchar() != '\n' )
     {
-        printf("Press 'F' to pay respect to Pushkin\n");
+        printf("Press ENTER to pay respect to Pushkin\n");
+    }
+
+    printf("How do you want to sort novel?\n"
+           "Type 'b' to sort from the beginning or 'e' to sort from the end\n");
+
+    scanf("%c", &input_check);
+    while ( (input_check != 'b') && (input_check != 'e'))
+    {
+        printf("This program can't sort novel like |%c|\n", input_check);
+        printf("Please, try again\n");
+        scanf("%c", &input_check);
+    }
+
+    if (input_check == 'b')
+    {
+        sort_type = 1;
+    } else
+    {
+        sort_type = -1;
     }
 
     printf("Now enter file name, where the novel is\n");
@@ -433,7 +581,7 @@ int main ()
     printf("Where do you want to put the modified novel?\n");
     scanf("%s", name_of_output_file);
 
-    file_sort(name_of_input_file, name_of_output_file);
+    file_sort(name_of_input_file, name_of_output_file, sort_type);
 
     return 0;
 }
